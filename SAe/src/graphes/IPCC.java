b39341c75acef9a63fcd5b65b5faa22667ef034e
flore <flore@192.168.1.31>
2022-05-15 19:49:35 +0200

    commentaires

--- SAe/src/graphes/IPCC.java
+++ SAe/src/graphes/IPCC.java
@@ -3,6 +3,7 @@ package graphes;
 import java.util.List;
 
 public interface IPCC {
-
+	
+	// calcule le plus court chemin
 	int pc(IGraphe graphe, int sommetDepart, int sommetArrivee, List<Integer> cheminPlusCourt);
 }
