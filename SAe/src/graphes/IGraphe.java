c522bec61be5b91779e630bc6ba1a3cd0fb74097
flore <flore@192.168.1.31>
2022-05-15 20:25:02 +0200

    test

--- SAe/src/graphes/IGraphe.java
+++ SAe/src/graphes/IGraphe.java
@@ -4,25 +4,56 @@ import java.util.List;
 
 public interface IGraphe {
 	final static int INFINI = Integer.MAX_VALUE;
-
-	// renvoie le nombre de sommet
+	
+	/**
+	 * revoie le nombre de sommet
+	 * @return le nombre de sommet
+	 */
 	int getNbSommets();
 	
-	// ajoute un arc au graphe
+	
+	/**
+	 * ajoute un arc au graphe
+	 * @param source : sommet de départ
+	 * @param valuation : la valeur de l'arc
+	 * @param destination : sommet d'arrivée
+	 */
 	void ajouterArc(int source, int valuation, int destination);
 	
-	// renvoie la valeur d'un arc entre dexu sommets
+	/**
+	 * revoie la valeur de l'arc
+	 * @param i : sommet de départ
+	 * @param j : sommet d'arriver
+	 * @return la valeur de l'arc
+	 */
 	int getValuation(int i, int j);
 	
-	// revoie si oui ou non il y a un arc entre les deux sommets
+	/**
+	 * Savoir si oui ou non il y a un arc entre deux sommets
+	 * @param i : sommet de départ
+	 * @param j : sommet d'arrivée
+	 * @return true ou false en focntion de s'il y a un arc ou pas
+	 */
 	boolean aArc(int i, int j);
 	
-	// renvoie la liste des successeurs
+	/**
+	 * Savoir les successeurs d'un sommet
+	 * @param noeud : sommet sur lequel on veut avoir les successeurs
+	 * @return une liste de successeurs
+	 */
 	List<Integer> listSuccesseurs(int noeud);
 	
-	// renvoie la liste des prédecesseurs 
+	/**
+	 * Savoir les prédécesseurs d'un sommet
+	 * @param noeud : sommet sur lequel on veut avoir les prédécesseurs
+	 * @return : une liste de prédécesseurs
+	 */
 	List<Integer> listPredecesseur(int noeud);
 	
-	// renvoie la distance du chemin envoyé
+	/**
+	 * Savoir la distance d'un chemin
+	 * @param cheminCalcule : list de sommet sur laquel on veut la distance
+	 * @return : la distance du chemin
+	 */
 	int distance(List<Integer> cheminCalcule);
 }
--- SAe/src/graphes/IPCC.java
+++ SAe/src/graphes/IPCC.java
@@ -4,6 +4,13 @@ import java.util.List;
 
 public interface IPCC {
 	
-	// calcule le plus court chemin
+	/**
+	 * Calcule le plus court chemin
+	 * @param graphe : graphe sur lequel le programme va travailler
+	 * @param sommetDepart : sommet de départ
+	 * @param sommetArrivee : sommet d'arrivée
+	 * @param cheminPlusCourt : le plus court chemin trouvé
+	 * @return : la longueur du plus court chemin trouvé
+	 */
 	int pc(IGraphe graphe, int sommetDepart, int sommetArrivee, List<Integer> cheminPlusCourt);
 }
